7f6b634c6b34cb9aa9efb6fa30eb411e6e7dc2ad
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-05-03 16:13:18 +0200

    basic setup

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Hero from '../Components/Hero';
+import Footer from '../Components/Footer';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
