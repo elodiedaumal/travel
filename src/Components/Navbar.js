100d4f383e46669612e1bc6fe7e9277b9caf7976
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-05-03 18:11:50 +0200

    toggle the menu in mobile

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,39 +1,76 @@
-import styled from 'styled-components';
+import { useState } from 'react';
+import styled, { css } from 'styled-components';
+
 import { MdOutlineTravelExplore } from 'react-icons/md';
 import { AiFillCloseCircle, AiOutlineMenuFold } from 'react-icons/ai';
 
 import { NavLinks } from '../data';
 
 const Navbar = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const handleSidebar = () => {
+    setSidebar(!sidebar);
+  };
+
   return (
     <nav>
       <Header className='flex'>
         <div>
           <Logo href='/' className='flex'>
             <h1>
-              <MdOutlineTravelExplore className='icon' /> Travel.
+              <MdOutlineTravelExplore className='icon iconLogo' />
+              Travel.
             </h1>
           </Logo>
         </div>
-        <FullNavLinks>
-          <NavLists className='flex'>
-            {NavLinks.map((link) => {
-              return (
-                <NavItem key={link.id}>
-                  <NavLinka href={link.link}>{link.name}</NavLinka>
-                </NavItem>
-              );
-            })}
-            <Button className='btn'>
-              <a href='/'>BOOK NOW</a>
-            </Button>
-          </NavLists>
-          <CloseBtn>
-            <AiFillCloseCircle className='icon' />
-          </CloseBtn>
-        </FullNavLinks>
+        {!sidebar ? (
+          <FullNavLinks>
+            <NavLists className='flex'>
+              {NavLinks.map((link) => {
+                return (
+                  <NavItem key={link.id}>
+                    <NavLinka href={link.link} onClick={handleSidebar}>
+                      {link.name}
+                    </NavLinka>
+                  </NavItem>
+                );
+              })}
+              <Button className='btn'>
+                <a href='/'>BOOK NOW</a>
+              </Button>
+            </NavLists>
+            <CloseBtn>
+              <AiFillCloseCircle className='icon' onClick={handleSidebar} />
+            </CloseBtn>
+          </FullNavLinks>
+        ) : (
+          <FullNavLinks $show>
+            <NavLists className='flex'>
+              {NavLinks.map((link) => {
+                return (
+                  <NavItem key={link.id}>
+                    <NavLinka href={link.link} onClick={handleSidebar}>
+                      {link.name}
+                    </NavLinka>
+                  </NavItem>
+                );
+              })}
+              <Button className='btn'>
+                <a href='/'>BOOK NOW</a>
+              </Button>
+            </NavLists>
+            <CloseBtn>
+              <AiFillCloseCircle className='icon' onClick={handleSidebar} />
+            </CloseBtn>
+          </FullNavLinks>
+        )}
+
         <div>
-          <AiOutlineMenuFold className='icon' />
+          <AiOutlineMenuFold
+            className='icon menuIcon'
+            onClick={handleSidebar}
+          />
         </div>
       </Header>
     </nav>
@@ -44,14 +81,19 @@ export default Navbar;
 
 const Header = styled.header`
   position: fixed;
-  justify-content: center;
+  justify-content: space-between;
   background: var(--WhiteColorDeam);
   width: 100%;
   padding: 1rem;
   z-index: 1;
+  box-shadow: 0 2px 4px rgba(2, 15, 29, 0.904);
 `;
 const Logo = styled.a`
   color: var(--BlackColor);
+  font-weight: 600;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
 `;
 
 const FullNavLinks = styled.div`
@@ -61,12 +103,17 @@ const FullNavLinks = styled.div`
     height: max-content;
     width: 80%;
     border-radius: 1rem;
-    top: 6rem;
+    top: -500rem;
     left: 50%;
     transform: translate(-50%);
     z-index: 999;
     box-shadow: 0 2px 4px rgba(2, 15, 29, 0.904);
-    transition: all 0.3s ease-in-out;
+    transition: all 1s ease-in-out;
+    ${(props) =>
+      props.$show &&
+      css`
+        top: 7rem;
+      `}
   }
 `;
 
